afa002a394b96a9cb27b28783e53bdf6319cfd14
brennan!  for reals <brennan.low@alaska.gov>
2015-10-14 18:09:16 -1000

    Create convert.py
    miles, km, years, minutes, inches, cm

---
+++ src/convert.py
@@ -0,0 +1,19 @@
+def kilometers_to_miles(value):
+    return.value / 1.609
+    
+def miles_to_kilometers(value):
+    return value * 1.609
+    
+def years_to_minutes(value)
+    return value * 365 * 24 * 60
+    
+def minutes_to_years(value)
+    return value / (365 * 24 *60)
+    
+def inches_to_cm(value)
+    return value / 2.54
+    
+def cm_to_inches(value)
+    return value * 2.54
+    
+#fin
