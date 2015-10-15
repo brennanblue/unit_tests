ac180ba0e1a6447ad711731f374c3ffe020db323
brennan!  for reals <brennan.low@alaska.gov>
2015-10-14 18:17:50 -1000

    Update convert.py
    added missing ':'s

--- src/convert.py
+++ src/convert.py
@@ -4,16 +4,16 @@ def kilometers_to_miles(value):
 def miles_to_kilometers(value):
     return value * 1.609
     
-def years_to_minutes(value)
+def years_to_minutes(value):
     return value * 365 * 24 * 60
     
-def minutes_to_years(value)
+def minutes_to_years(value):
     return value / (365 * 24 *60)
     
-def inches_to_cm(value)
+def inches_to_cm(value):
     return value / 2.54
     
-def cm_to_inches(value)
+def cm_to_inches(value):
     return value * 2.54
     
 #fin
