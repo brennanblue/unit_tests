c50e434a290302574c5bcb732c5a3d54608a73e4
brennan!  for reals <brennan.low@alaska.gov>
2015-10-19 17:37:00 -1000

    Fixed ID10T error

--- src/convert.py
+++ src/convert.py
@@ -11,9 +11,9 @@ def minutes_to_years(value):
     return value / (365 * 24 *60)
     
 def inches_to_cm(value):
-    return value / 2.54
+    return value *  2.54
     
 def cm_to_inches(value):
-    return value * 2.54
+    return value / 2.54
     
 #fin
