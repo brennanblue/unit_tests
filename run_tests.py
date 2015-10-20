c4c16346e69ef0704ddb60c38731073b37b3fe06
brennan!  for reals <brennan.low@alaska.gov>
2015-10-19 16:49:12 -1000

    expanded unit tests for cm, inches conversion

--- run_tests.py
+++ run_tests.py
@@ -7,9 +7,9 @@ years_to_minutes, minutes_to_years, inches_to_cm, cm_to_inches
 
 class TestConvert(unittest.TestCase):
     def test_km_to_miles(self):
-        acutal = kilometers_to_miles(1)
+        actual = kilometers_to_miles(1)
         expected = 0.621 # general knowledge
-        #self.assertAlmostEqual(acutal, expected, delta = 0.01)
+        self.assertAlmostEqual(actual, expected, delta = 0.01)
         
     def test_miles_to_km(self):
         actual = miles_to_kilometers(1)
@@ -24,10 +24,14 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(1, minutes_to_years(525600))
 
     def test_inches_to_cm(self):
-        self.assertEqual(2.54, inches_to_cm(1))
+        actual = inches_to_cm(1)
+        expected = 2.54
+        self.assertEqual(actual, expected)
         
     def test_cm_to_inches(self):
-        self.assertAlmostEqual(1, cm_to_inches(1), delta = 0.05)
+        actual = cm_to_inches(1)
+        expected = .3937
+        self.assertAlmostEqual(actual, expected, delta = 0.05)
 
 #######################################################
 
